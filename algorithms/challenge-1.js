74a8af4fe698c88dc938cf33a9276dd6826e1b70
iykeafrica <ikechiucheagwu@gmail.com>
2022-06-19 19:04:31 +0100

    One and Four questions updated

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,40 +1,8 @@
 //Implement a function secondLargest(arr) that takes an array of numbers and returns the second largest number.
 function secondLargest(arr) {
 
-  if (arr.length == 2) {
-    if (arr[0] > arr[1]) {
-      return arr[1];
-    } else {
-      return arr[0];
-    }
-  }
-
-  let largestNum = arr[0];
-
-  for (let i = 0; i < arr.length; i++) {
-    if (largestNum >= arr[i]) {
-      continue;
-    } else {
-      largestNum = arr[i];
-    }
-  }
-
-  let secondLargestNum = arr[0];
-  if (largestNum == arr[0]) {
-    secondLargestNum = arr[1];
-  }
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == largestNum) {
-      continue;
-    } else {
-      if (secondLargestNum >= arr[i]) {
-        continue;
-      } else {
-        secondLargestNum = arr[i];
-      }
-    }
-  }
+  const sortedArray = arr.sort((a, b) => (a - b));
+  let secondLargestNum = sortedArray[sortedArray.length - 2];
 
   console.log(secondLargestNum);
   return secondLargestNum;
--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,40 +1,16 @@
 //Implement the function sumTwoSmallestNums(arr) that takes an array of numbers and returns the sum of the two lowest positive numbers.
 function sumTwoSmallestNums(arr) {
-    let lowestNum = arr[0];
-    let lowestPostion = 0;
-    
-    for (let i = 0; i < arr.length; i++) {
-        if(lowestNum <= arr[i] && lowestNum >= 0) {
-            continue;
-        } else {
-            if (arr[i] >= 0) {
-                lowestNum = arr[i];
-                lowestPostion = i;
-            }
+    const sortedArray = arr.sort((a, b) => (a - b));
+    let positiveArray = [];
+
+    for (let i = 0; i < sortedArray.length; i++) {
+        if (sortedArray[i] >= 0) {
+            positiveArray.push(sortedArray[i]);
         }
     }
 
-    let secondLowestNum = arr[0];
-    
-    if (lowestPostion == 0) {
-        secondLowestNum = arr[1];
-    }
-
-    for (let i = 0; i < arr.length; i++) {
-        if (i == lowestPostion) {
-            continue;
-        } else {
-            if(secondLowestNum <= arr[i] && secondLowestNum >= 0) {
-                continue;
-            } else {
-                if (arr[i] >= 0) {
-                    secondLowestNum = arr[i];
-                }
-            }
-        }
-    }
-
-    console.log(lowestNum + secondLowestNum);
-    return (lowestNum + secondLowestNum);
+    let result = positiveArray[0] + positiveArray[1];
+    console.log(result);
+    return result;
 }
 module.exports = sumTwoSmallestNums;
